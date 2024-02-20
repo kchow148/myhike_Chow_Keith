b6d850d241f5db9f1ecbd2bf7284af43c751e270
kchow148 <kfc071997@hotmail.com>
2024-02-20 10:08:24 -0800

    demo 7

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyC789jyXKufyuBTL4kvcR7cF5ODMDwKJSc",
+    authDomain: "comp1800-202410-demo-424e1.firebaseapp.com",
+    projectId: "comp1800-202410-demo-424e1",
+    storageBucket: "comp1800-202410-demo-424e1.appspot.com",
+    messagingSenderId: "314032407991",
+    appId: "1:314032407991:web:b9b4d31f12538455b71432"
 };
 
 //--------------------------------------------
